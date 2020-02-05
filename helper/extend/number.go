5dee05461af8689c22e0b7eb7674da861c348934
yuxinyun <mryu1122@163.com>
2020-02-05 09:59:42 +0800

    # add registerAndLogin example

--- helper/extend/number.go
+++ helper/extend/number.go
@@ -1,5 +1,28 @@
 package extend
 
+func InterfaceToInt(inter interface{}) (int, bool) {
+	switch inter.(type) {
+	case int:
+		{
+			return inter.(int), true
+		}
+	case int64:
+		{
+			return int(inter.(int64)), true
+		}
+	case float64:
+		{
+			return int(inter.(float64)), true
+		}
+	case float32:
+		{
+			return int(inter.(float32)), true
+		}
+	}
+
+	return 0, false
+}
+
 func InterfaceToInt64(inter interface{}) (int64, bool) {
 	switch inter.(type) {
 	case int:
